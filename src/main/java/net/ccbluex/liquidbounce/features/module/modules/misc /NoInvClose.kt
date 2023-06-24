67b7dd7d81966cb558919c9dad2b3c9402eb0838
zxczenos138 <133138811+zxczenos138@users.noreply.github.com>
2023-06-24 20:16:54 +0500

    NoInvClose.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc /NoInvClose.kt
@@ -0,0 +1,22 @@
+/*
+ * SilenceClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/ZNmRtClient/SilenceClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.misc
+
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.minecraft.network.play.server.S2EPacketCloseWindow
+import net.minecraft.client.gui.inventory.GuiInventory
+
+object NoInvClose : Module(name = "NoInvClose", category = ModuleCategory.MISC, defaultOn = false) {
+    @EventTarget
+    fun onPacket(event: PacketEvent){
+        if (mc.theWorld == null || mc.thePlayer == null) return
+        
+        if (event.packet is S2EPacketCloseWindow && mc.currentScreen is GuiInventory) event.cancelEvent()
+    }
+}

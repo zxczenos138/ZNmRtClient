c94b4001fca44dbcaa84846350c5b3f72278cb23
zxczenos138 <133138811+zxczenos138@users.noreply.github.com>
2023-06-24 18:35:34 +0500

    ScriptGUI.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/Silence /ScriptGUI.kt
@@ -0,0 +1,16 @@
+/*
+ * SilenceClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/ZNmRtClient/SilenceClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.client
+
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.ui.client.gui.modernui.GuiScriptLoadMenu
+
+object ScriptGUI : Module("ScriptGUI", category = ModuleCategory.CLIENT, canEnable = false, defaultOn = false) {
+    override fun onEnable() {
+        mc.displayGuiScreen(GuiScriptLoadMenu())
+    }
+}

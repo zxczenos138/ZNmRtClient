1ca07c515f1e211175a30cb0d8c84b7266646ec7
zxczenos138 <133138811+zxczenos138@users.noreply.github.com>
2023-06-24 21:41:11 +0500

    NoSlowBreak.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/world /NoSlowBreak.kt
@@ -0,0 +1,15 @@
+/*
+ * SilenceClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/ZNmRtClient/SilenceClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.world
+
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.value.BoolValue
+
+object NoSlowBreak : Module(name = "NoSlowBreak", category = ModuleCategory.WORLD, defaultOn = false) {
+    val airValue = BoolValue("Air", true)
+    val waterValue = BoolValue("Water", false)
+}

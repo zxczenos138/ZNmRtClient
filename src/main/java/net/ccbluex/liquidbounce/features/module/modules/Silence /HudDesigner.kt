a823d683c239e4011e27cc17fd2c438791afad97
zxczenos138 <133138811+zxczenos138@users.noreply.github.com>
2023-06-24 18:32:55 +0500

    HudDesigner.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/Silence /HudDesigner.kt
@@ -0,0 +1,16 @@
+/*
+ * SilenceClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/ZNmRtClient/SilenceClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.client
+
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.ui.client.hud.designer.GuiHudDesigner
+
+object HudDesigner : Module("HudDesigner", category = ModuleCategory.CLIENT, canEnable = false, defaultOn = false) {
+    override fun onEnable() {
+        mc.displayGuiScreen(GuiHudDesigner())
+    }
+}

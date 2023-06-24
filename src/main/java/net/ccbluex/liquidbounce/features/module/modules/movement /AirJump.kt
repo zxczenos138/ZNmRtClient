331230dd84ba67731abe5311a5ef9f3a405f3175
zxczenos138 <133138811+zxczenos138@users.noreply.github.com>
2023-06-24 20:47:34 +0500

    AirJump.kt

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement /AirJump.kt
@@ -0,0 +1,32 @@
+/*
+ * SilenceClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/ZNmRtClient/SilenceClient/
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement
+
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.value.BoolValue
+import net.minecraft.network.play.client.C03PacketPlayer
+
+object AirJump : Module(name = "AirJump", category = ModuleCategory.MOVEMENT, defaultOn = false) {
+    private val spoofGroundValue = BoolValue("SpoofGround", false)
+    @EventTarget
+    fun onUpdate(event: UpdateEvent) {
+        mc.thePlayer.onGround = true
+    }
+
+    @EventTarget
+    fun onPacket(event: PacketEvent) {
+        val packet = event.packet
+        if(packet is C03PacketPlayer) {
+            if(spoofGroundValue.get()) {
+                packet.onGround = true
+            }
+        }
+    }
+}

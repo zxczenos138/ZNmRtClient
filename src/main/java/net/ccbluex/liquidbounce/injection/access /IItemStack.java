30b18db12537c02fb30953e2382520cfa356a3d7
zxczenos138 <133138811+zxczenos138@users.noreply.github.com>
2023-06-24 23:10:15 +0500

    IItemStack.java

---
+++ src/main/java/net/ccbluex/liquidbounce/injection/access /IItemStack.java
@@ -0,0 +1,10 @@
+/*
+ * SilenceClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/ZNmRtClient/SilenceClient/
+ */
+package net.ccbluex.liquidbounce.injection.access;
+
+public interface IItemStack {
+    long getItemDelay();
+}
